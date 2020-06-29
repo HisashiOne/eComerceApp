713871fca7bd0cb4fd4626217fcf453ab37b1d78
Oswaldo Morales <vgoswaldo@gmail.com>
2020-06-28 20:22:33 -0500

    StoryBoard Navigation

--- eComerceApp/ViewControllers/LoginViewController.swift
+++ eComerceApp/ViewControllers/LoginViewController.swift
@@ -12,19 +12,25 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.setGradientBackground()
 
         // Do any additional setup after loading the view.
     }
     
+    
+    func setGradientBackground() {
+          let colorTop = UIColor(red: 0.98, green: 0.82, blue: 0.76, alpha: 1.00).cgColor
+          let colorBottom = UIColor(red: 0.83, green: 0.77, blue: 0.98, alpha: 1.00).cgColor
+      
+          let gradientLayer = CAGradientLayer()
+          gradientLayer.colors = [colorTop, colorBottom]
+          gradientLayer.locations = [0.0, 1.0]
+          gradientLayer.frame = self.view.bounds
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+          self.view.layer.insertSublayer(gradientLayer, at:0)
+      }
+      
+    
 
 }
